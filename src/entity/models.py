fc4ec21329b5351abaadf76cc67dfd682b89edb8
GodVov4 <volume245@gmail.com>
2023-12-23 17:09:54 +0200

    refactor with fastapi-users

--- src/entity/models.py
+++ src/entity/models.py
@@ -1,6 +1,7 @@
 import enum
 
 from datetime import date
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID, generics
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Date, Enum, DateTime, func, Integer, ForeignKey
 from sqlalchemy.orm import DeclarativeBase
@@ -22,7 +23,7 @@ class Contact(Base):
     created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now(), nullable=True)
     updated_at: Mapped[date] = mapped_column('updated_at', DateTime, default=func.now(), onupdate=func.now(),
                                              nullable=True)
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), nullable=True)
+    user_id: Mapped[generics.GUID] = mapped_column(generics.GUID(), ForeignKey('user.id'), nullable=True)
     user: Mapped["User"] = relationship("User", backref="contacts", lazy="joined")
 
 
@@ -32,12 +33,8 @@ class Role(enum.Enum):
     user: str = "user"
 
 
-class User(Base):
-    __tablename__ = 'users'
-    id: Mapped[int] = mapped_column(primary_key=True)
+class User(SQLAlchemyBaseUserTableUUID, Base):
     username: Mapped[str] = mapped_column(String(50))
-    email: Mapped[str] = mapped_column(String(150), nullable=False, unique=True)
-    password: Mapped[str] = mapped_column(String(255), nullable=False)
     avatar: Mapped[str] = mapped_column(String(255), nullable=True)
     refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
     created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now())
